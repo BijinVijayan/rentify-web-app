1d95432c3ee5e9756c6297ebb199886e80cd167e
bijin ev <bijinev55@gmail.com>
2025-11-23 17:13:26 +0530

    added login page

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginHost from "@/components/LoginHost";
+
+function SignIn() {
+    return (
+        <div>
+            <LoginHost/>
+        </div>
+    )
+}
+export default SignIn
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -44,7 +44,7 @@ const TestimonialCard = ({ data }: { data: Testimonial }) => {
                     src={data.imageSrc}
                     alt={data.imageAlt}
                     fill
-                    className="object-cover transition-transform duration-500 group-hover:scale-105"
+                    className="object-cover object-top transition-transform duration-500 group-hover:scale-105"
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
             </div>
